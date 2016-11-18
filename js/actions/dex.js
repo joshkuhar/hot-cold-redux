6a4d98d7d36c0979605fcf46aee881391ac1abed
Josh Kuhar <jdkuhar@gmail.com>
2016-11-17 20:28:49 -0500

    endpoints are live

--- js/actions/dex.js
+++ js/actions/dex.js
@@ -1,3 +1,5 @@
+require('isomorphic-fetch');
+
 var REPORT_TEMP = 'REPORT_TEMP';
 var reportTemp = function(temp) {
 	return {
@@ -5,6 +7,90 @@ var reportTemp = function(temp) {
 		guess: temp
 	}
 };
-
 exports.REPORT_TEMP = REPORT_TEMP;
 exports.reportTemp = reportTemp;
+
+var FETCH_SUCCESS = 'FETCH_SUCCESS';
+var fetchSuccess = function(answer) {
+    return {
+        type: FETCH_SUCCESS,
+        answer: answer
+    };
+};
+
+var FETCH_ERROR= 'FETCH_ERROR';
+var fetchError = function(repository, error) {
+    console.log(error);
+    return {
+        type: FETCH_ERROR,
+        repository: repository,
+        error: error
+    };
+};
+
+exports.FETCH_SUCCESS = FETCH_SUCCESS;
+exports.fetchSuccess = fetchSuccess;
+exports.FETCH_ERROR = FETCH_ERROR;
+exports.fetchError = fetchError;
+/*
+data model
+{guesses: [3, 4, 5]}
+{guess: "2"}
+{"reply": "Not the lowest"}
+*/
+
+var fetchAnswer = function() {
+    return function(dispatch) {
+        var url = 'http://localhost:8080/fewest-guesses';
+        return fetch(url)  
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data)
+        })
+        .catch(function(error) {
+            console.log(error);
+            return dispatch(
+                fetchError(error)
+            );
+        });
+    }
+};
+exports.fetchAnswer = fetchAnswer;
+
+var postAnswer = function() {
+    return function(dispatch) {
+        var guess = {guess: "21"};
+        var url = 'http://localhost:8080/fewest-guesses';
+        return fetch(url, {
+        method: "POST",
+        body: JSON.stringify(guess),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(function(res) {
+        return res.json()
+    }).then(function(data) {
+        console.log(data)
+    }).catch(function(error) {
+        return dispatch(
+            fetchError(error)
+            );
+        });
+    }
+};
+exports.postAnswer = postAnswer;
+
+
+
+
+
+
+
+
+
+
+
+
+
